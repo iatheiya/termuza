cd4dde6efaa8f5735c8ddc97b04d09eda7c75e22
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-01 21:15:12 +0100

    Update MessageDialogUtils.java

--- termux-shared/src/main/java/com/termux/shared/interact/MessageDialogUtils.java
+++ termux-shared/src/main/java/com/termux/shared/interact/MessageDialogUtils.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AlertDialog;
 
 import com.termux.shared.R;
