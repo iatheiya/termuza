7ccb96c431ee2598a0040701e35d6605fe2e9973
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-02 14:59:47 +0100

    Update Android.mk

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -6,6 +6,8 @@ BOOTSTRAP_SRC := termux-bootstrap-zip.S
 
 LOCAL_SRC_FILES := $(BOOTSTRAP_SRC) termux-bootstrap.c
 LOCAL_MODULE := libtermux-bootstrap
-LOCAL_CFLAGS += -Os -fno-stack-protector
+LOCAL_CFLAGS += -Os -fno-stack-protector -I$(LOCAL_PATH)
+LOCAL_ASFLAGS += -I$(LOCAL_PATH)
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 
 include $(BUILD_SHARED_LIBRARY)
