62f40741046ba77b46b2b0bc14216c55a8bcf6f0
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-02 17:31:46 +0100

    Update Android.mk

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -6,8 +6,10 @@ BOOTSTRAP_SRC := termux-bootstrap-zip.S
 
 LOCAL_SRC_FILES := $(BOOTSTRAP_SRC) termux-bootstrap.c
 LOCAL_MODULE := libtermux-bootstrap
-LOCAL_CFLAGS += -Os -fno-stack-protector -I$(LOCAL_PATH) -fvisibility=hidden
+LOCAL_CFLAGS += -Os -fno-stack-protector -I$(LOCAL_PATH) -fvisibility=hidden -Wall -Wextra -std=c11
 LOCAL_ASFLAGS += -I$(LOCAL_PATH)
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_ARM_MODE := arm
+LOCAL_LDFLAGS += -Wl,--no-undefined
 
 include $(BUILD_SHARED_LIBRARY)
