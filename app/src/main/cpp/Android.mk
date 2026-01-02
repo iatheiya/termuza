d53a0e3784dd5d193798b106e7f74feef27db537
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-02 13:50:57 +0100

    Update Android.mk

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -1,5 +1,18 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
+
+ifeq ($(TARGET_ARCH_ABI), arm64-v8a)
+BOOTSTRAP_SRC := termux-bootstrap-zip-arm64-v8a.S
+else ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
+BOOTSTRAP_SRC := termux-bootstrap-zip-armeabi-v7a.S
+else ifeq ($(TARGET_ARCH_ABI), x86)
+BOOTSTRAP_SRC := termux-bootstrap-zip-x86.S
+else ifeq ($(TARGET_ARCH_ABI), x86_64)
+BOOTSTRAP_SRC := termux-bootstrap-zip-x86_64.S
+else
+BOOTSTRAP_SRC := termux-bootstrap-zip.S
+endif
+
+LOCAL_SRC_FILES := $(BOOTSTRAP_SRC) termux-bootstrap.c
 LOCAL_MODULE := libtermux-bootstrap
-LOCAL_SRC_FILES := termux-bootstrap-zip.S termux-bootstrap.c
 include $(BUILD_SHARED_LIBRARY)
